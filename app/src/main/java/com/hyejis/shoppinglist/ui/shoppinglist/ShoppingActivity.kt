7fa805316ac1678a98167d8c3b8e405137505aba
hyejiseo-dev <dianna537@gmail.com>
2022-03-19 15:33:16 +0900

    kodein 사용해 applicaion 생성, singleton 저장소 반영

--- app/src/main/java/com/hyejis/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/hyejis/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -11,15 +11,23 @@ import com.hyejis.shoppinglist.db.entities.ShoppingItem
 import com.hyejis.shoppinglist.other.ShoppingItemAdapter
 import com.hyejis.shoppinglist.repository.ShoppingRepository
 import kotlinx.android.synthetic.main.activity_shopping.*
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class ShoppingActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity(), KodeinAware {
+
+    override val kodein: Kodein by kodein()
+    private val factory: ShoppingViewModelFactory by instance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
 
-        val database = ShoppingDatabase(this)
-        val repository = ShoppingRepository(database)
-        val factory = ShoppingViewModelFactory(repository)
+//        val database = ShoppingDatabase(this)
+//        val repository = ShoppingRepository(database)
+//        val factory = ShoppingViewModelFactory(repository)
 
         val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
 
